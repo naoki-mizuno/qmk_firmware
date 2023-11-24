22a6357ceea32e50f8bb74f0133bfb419381d293
Naoki Mizuno <naoki.mizuno.256@gmail.com>
2023-11-25 00:21:14 +0900

    Fix incorrect layer order

--- keyboards/hhkb/yang/keymaps/naomiz/keymap.c
+++ keyboards/hhkb/yang/keymaps/naomiz/keymap.c
@@ -24,9 +24,9 @@
 
 enum custom_layers {
     BASE,
+    QWERTY,
     HHKB,
     MOUSE,
-    QWERTY,
 };
 
 #define BATTERY_FULL 556
@@ -94,8 +94,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC , KC_1   , KC_2         , KC_3   , KC_4   , KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
             KC_TAB , KC_QUOT, KC_COMM      , KC_DOT , KC_P   , KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_EQL , KC_BSPC,
             KC_LCTL, KC_A   , KC_O         , KC_E   , KC_U   , KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, KC_ENT ,
-            KC_LSFT, KC_SCLN, KC_Q         , KC_J   , KC_K   , KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, MO(2)  ,
-            MO(1)  , SC_LAPO, SFT_T(KC_SPC), SC_RAPC, KC_RGUI
+            KC_LSFT, KC_SCLN, KC_Q         , KC_J   , KC_K   , KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, MO(3)  ,
+            MO(2)  , SC_LAPO, SFT_T(KC_SPC), SC_RAPC, KC_RGUI
+    ),
+
+    /* QWERTY Level: QWERTY layout
+     +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+     | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  |  `  |
+     |-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-----+
+     | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  | BackSp |
+     |--------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+--------+
+     | Control  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |      Enter |
+     |----------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+------+-----+
+     | L_Shift    |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  R_Shift | Ly0 |
+     +--------+---+-+---+-----++----+-----+-----+-----+-----+----++-----+---+-+----+-----+-----+
+              | Ly1 |   LAlt   |              Space              |   RAlt   | RGUI |
+              +-----+----------+---------------------------------+----------+------+
+    */
+    [QWERTY] = LAYOUT_60_hhkb(
+            KC_ESC , KC_1   , KC_2  , KC_3   , KC_4   , KC_5, KC_6, KC_7, KC_8   , KC_9  , KC_0   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,
+            KC_TAB , KC_Q   , KC_W  , KC_E   , KC_R   , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
+            KC_LCTL, KC_A   , KC_S  , KC_D   , KC_F   , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN, KC_QUOT, KC_ENT ,
+            KC_LSFT, KC_Z   , KC_X  , KC_C   , KC_V   , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, DF(0)  ,
+            MO(2)  , KC_LALT, KC_SPC, KC_RALT, KC_RGUI
     ),
 
     /* Layer HHKB: HHKB mode (HHKB Fn)
@@ -116,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_PWR , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_INS , KC_DEL,
             KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP  , KC_TRNS, KC_BSPC,
             KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_VBAT,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END , KC_PGDN, KC_DOWN, KC_TRNS, DF(3)  ,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END , KC_PGDN, KC_DOWN, KC_TRNS, DF(1)  ,
             KC_TRNS, KC_TRNS, KC_LOCK, KC_TRNS, KC_TRNS
     ),
 
@@ -141,26 +162,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, DM_PLY1, DM_PLY2, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_RGHT, KC_TRNS,
             KC_TRNS, KC_WH_L, KC_BTN3, KC_WH_R, KC_WH_D, DM_RSTP, DM_RSTP, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_LOCK, KC_TRNS, KC_TRNS
-    ),
-
-    /* QWERTY Level: QWERTY layout
-     +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-     | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  |  `  |
-     |-----+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+-----+
-     | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  | BackSp |
-     |--------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+--------+
-     | Control  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |      Enter |
-     |----------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+------+-----+
-     | L_Shift    |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  R_Shift | Ly0 |
-     +--------+---+-+---+-----++----+-----+-----+-----+-----+----++-----+---+-+----+-----+-----+
-              | Ly1 |   LAlt   |              Space              |   RAlt   | RGUI |
-              +-----+----------+---------------------------------+----------+------+
-    */
-    [QWERTY] = LAYOUT_60_hhkb(
-            KC_ESC , KC_1   , KC_2  , KC_3   , KC_4   , KC_5, KC_6, KC_7, KC_8   , KC_9  , KC_0   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,
-            KC_TAB , KC_Q   , KC_W  , KC_E   , KC_R   , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
-            KC_LCTL, KC_A   , KC_S  , KC_D   , KC_F   , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN, KC_QUOT, KC_ENT ,
-            KC_LSFT, KC_Z   , KC_X  , KC_C   , KC_V   , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, DF(0)  ,
-            MO(1)  , KC_LALT, KC_SPC, KC_RALT, KC_RGUI
     ),
 };
