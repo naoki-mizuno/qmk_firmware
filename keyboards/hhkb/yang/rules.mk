ca65f2494bdb52ae6e5295a62453c23d2686c709
Naoki Mizuno <naoki.mizuno.256@gmail.com>
2025-11-30 19:26:03 +0900

    Use keyboard.json instead of rules.mk

--- keyboards/hhkb/yang/rules.mk
+++ keyboards/hhkb/yang/rules.mk
@@ -1,19 +1,6 @@
 # MCU frequency
 F_CPU = 8000000
 
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # USB Nkey Rollover
-COMBO_ENABLE = yes          # Enable combos
-DYNAMIC_MACRO_ENABLE = yes  # Enable dynamic macros
-MAGIC_ENABLE = no
-
 BLUETOOTH_ENABLE = yes
 
 LTO_ENABLE = yes
