21de7283274ad07195d5f86b5446d434b7fc9744
Naoki Mizuno <naoki.mizuno.256@gmail.com>
2025-11-30 09:16:01 +0900

    Change LALT and RALT to LAPO and RAPC (set to curly braces)
    This change is made so that it would be easier to navigate between
    paragraphs in Vim. I wasn't using the parentheses (original LAPO and
    RAPC) because it was simply faster to just press Shift-9 or Shift-0.
    
    On the other hand, I found that I use the curly braces `{` and `}` more
    often when navigating between code blocks.

--- keyboards/hhkb/yang/keymaps/naomiz/keymap.c
+++ keyboards/hhkb/yang/keymaps/naomiz/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      |----------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+------+-----+
      | L_Shift    |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  R_Shift | Ly0 |
      +--------+---+-+---+-----++----+-----+-----+-----+-----+----++-----+---+-+----+-----+-----+
-              | Ly1 |   LAlt   |              Space              |   RAlt   | RGUI |
+              | Ly1 |   LAPO   |              Space              |   RAPC   | RGUI |
               +-----+----------+---------------------------------+----------+------+
     */
     [QWERTY] = LAYOUT_60_hhkb(
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB , KC_Q   , KC_W  , KC_E   , KC_R   , KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
             KC_LCTL, KC_A   , KC_S  , KC_D   , KC_F   , KC_G, KC_H, KC_J, KC_K   , KC_L  , KC_SCLN, KC_QUOT, KC_ENT ,
             KC_LSFT, KC_Z   , KC_X  , KC_C   , KC_V   , KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, DF(0)  ,
-            MO(2)  , KC_LALT, KC_SPC, KC_RALT, KC_RGUI
+            MO(2)  , SC_LAPO, KC_SPC, SC_RAPC, KC_RGUI
     ),
 
     /* Layer HHKB: HHKB mode (HHKB Fn)
