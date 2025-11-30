efb80fad81f68528cbca5c0a629b54db9cbce6ec
Naoki Mizuno <naoki.mizuno.256@gmail.com>
2025-11-30 20:52:00 +0900

    Add wrappers to override weak stubs in bluetooth.c

--- drivers/bluetooth/bluefruit_le_uart.c
+++ drivers/bluetooth/bluefruit_le_uart.c
@@ -330,6 +330,31 @@ void bluefruit_le_send_keyboard(report_keyboard_t *report) {
     return;
 }
 
+// Wrapper functions to override weak stubs in bluetooth.c
+void bluetooth_init(void) {
+    bluefruit_le_init();
+}
+
+void bluetooth_task(void) {
+    bluefruit_le_task();
+}
+
+bool bluetooth_is_connected(void) {
+    return bluefruit_le_is_connected();
+}
+
+void bluetooth_send_keyboard(report_keyboard_t *report) {
+    bluefruit_le_send_keyboard(report);
+}
+
+void bluetooth_send_mouse(report_mouse_t *report) {
+    bluefruit_le_send_mouse(report);
+}
+
+void bluetooth_send_consumer(uint16_t usage) {
+    bluefruit_le_send_consumer(usage);
+}
+
 void bluefruit_le_send_consumer(uint16_t usage) {
     struct queue_item item;
 
